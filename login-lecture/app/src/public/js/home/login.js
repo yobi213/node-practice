6f6458c83ada4f7b6b96af6e5c11b2600993c806
yobi213 <yobi@yobis-MacBook-Pro.local>
2022-05-05 22:14:09 +0900

    DOM

--- login-lecture/app/src/public/js/home/login.js
+++ login-lecture/app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
 "use strict";
 
-console.log('hello');
-console.log('bye');
+const { login } = require("../../../home/home.ctrl");
+
+const id = document.querySelector("#id"),
+ psword = document.querySelector("#psword"),
+ loginBtn = document.querySelector("button");
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.ariaValueMax,
+        psword: psword.ariaValueMax,
+    };
+    console.log(req);
+}
